89c35ad93585b2cf963d95f9ec0e8b39ee0d3c1b
mthsxz <danilo.matheusduarte@gmail.com>
2024-07-17 22:04:38 -0300

    Atualiza funções de extrato, depósito e saque
    - Adiciona função `extrato` para exibir o saldo atual
    - Adiciona função `deposito` para adicionar valor ao saldo
    - Adiciona função `saque` para retirar valor do saldo
    - Melhora a estrutura do código separando funcionalidades em funções
    - Inclui validação para valores de depósito e saque

--- bank v1 .py
+++ bank v1 .py
@@ -1,53 +1,48 @@
-# Verifica o saldo atual
-saldo = 0
+def extrato(saldo):
+    print('Seu saldo atual é R$ {:.2f}'.format(saldo))
 
-# Variáveis para armazenar os contadores
-cont_depositos = 0
-cont_saques = 0
+def deposito(saldo):
+    valor_deposito = float(input('Digite o valor do depósito: R$ '))
+    if valor_deposito <= 0:
+        print('Digite um valor válido')
+    else:
+        saldo += valor_deposito
+        print('Depósito de R$ {:.2f} efetuado com sucesso'.format(valor_deposito))
+        print('Seu saldo atual é de R$ {:.2f}'.format(saldo))
+    return saldo
 
-# Exibindo o menu inicial
-print('\nMenu:')
-print('[1] Extrato')
-print('[2] Depósito')
-print('[3] Saque')
-print('[4] Sair')
+def saque(saldo):
+    print('Seu saldo atual é de R$ {:.2f}'.format(saldo))
+    valor_saque = float(input('Qual o valor que você deseja sacar? R$ '))
+    if valor_saque > saldo:
+        print('Não é possível realizar a transação, dinheiro insuficiente')
+    else:
+        saldo -= valor_saque
+        print('Saque efetuado com sucesso.')
+        print('Seu saldo atual é R$ {:.2f}'.format(saldo))
+    return saldo
+
+# Saldo inicial
+saldo = 0.0
 
 # Inicia o loop para verificar as opções
 while True:
-    opcao = int(input('Escolha uma opção (1/2/3/4): '))
-
-    # Começa a verificar a escolha do usuário
-    if opcao == 1:
-        print('Seu saldo atual é R$ {:.2f}'.format(saldo))
-
-    elif opcao == 2:
-        valor_deposito = float(input('Digite o valor do depósito: R$ '))
-        if valor_deposito <= 0:
-            print('Digite um valor válido')
-        else:
-            saldo += valor_deposito
-            cont_depositos += 1  # contador de depósitos
-            print('Depósito de {:.2f} efetuado com sucesso'.format(valor_deposito))
-            print('Seu saldo atual é de {:.2f}'.format(saldo))
-
-    elif opcao == 3:
-        valor_saque = float(input('Qual o valor do saque? R$ '))
-        if valor_saque > saldo:
-            print('Não é possível realizar a transação, dinheiro insuficiente')
-        else:
-            saldo -= valor_saque
-            cont_saques += 1  # contador de saques
-            print('Saque efetuado com sucesso.')
-            print('Seu saldo atual é {:.2f}'.format(saldo))
-
-    elif opcao == 4:
-        print('Fechando o programa...')
-        break  # Encerra o loop assim que o usuário escolhe a opção 4
-
+    print('\nMenu:')
+    print('[1] Extrato')
+    print('[2] Depósito')
+    print('[3] Saque')
+    print('[4] Sair')
+    escolha = int(input('Escolha uma opção (1/2/3/4): '))
+    
+    if escolha == 1:
+        extrato(saldo)
+    elif escolha == 2:
+        saldo = deposito(saldo)
+    elif escolha == 3:
+        saldo = saque(saldo)
+    elif escolha == 4:
+        print('Saindo do sistema...')
+        break
     else:
-        print('Opção inválida. Digite uma opção válida (1/2/3/4)')
-
-# Exibindo o total de operações realizadas na conta
-print(f'Total de depósitos realizados: {cont_depositos}')
-print(f'Total de saques realizados: {cont_saques}')
-
+        print('Escolha uma opção válida')
+        
